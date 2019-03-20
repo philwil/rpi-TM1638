d4e705d4840d1e4201e1dd7aa52f0a80b6276462
philwil <sysdcs@gmail.com>
2019-03-20 16:08:56 -0400

    first client starting

--- test/test_2_TM.py
+++ test/test_2_TM.py
@@ -1,3 +1,4 @@
+#this one connects to a port / socket
 # coding=utf-8
 from time import sleep
 from rpi_TM1638 import TMBoards
@@ -31,26 +32,87 @@ TM.segments[3,1] = True     # turn on the segment #1 of the 7-segment number 3
 #TM.segments[8] = '01234567'
 #TM.leds = (True, False, True)   # set the three first leds
 
+print __name__
+if __name__ == "__main__":
+    NODE,HOST,PORT = "LEDS1","pine64-001",5541
+    if len(sys.argv) >= 4:
+        print "Correct usage: script, NODEIP address, port number"
+        NODE= str(sys.argv[1])
+        HOST= str(sys.argv[2])
+        PORT = int(sys.argv[3])
 
-#
-l = 0
-lasta = 0
-while True:
-    TM.leds[l] = False
-    l = l+1
-    if l > 7:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((HOST, PORT))
+    
+    msg = "set " + NODE + ":led1 off\n"
+    server.send(msg)
+    print msg
+    msg = "set " + NODE + ":led2 off\n"
+    server.send(msg)
+    print msg
+    msg = "set " + NODE + ":led3 off\n"
+    server.send(msg)
+    print msg
+    msg = "set " + NODE + ":led4 off\n"
+    server.send(msg)
+    print msg
+    
+
+
+    
+    while True:
+        #
         l = 0
-    TM.leds[l] = True
+        lasta = 0
+        # maintains a list of possible input streams
+        sockets_list = [sys.stdin, server]
+    
+        """ There are two possible input situations. Either the
+        user wants to give manual input to send to other people,
+        or the server is sending a message to be printed on the
+        screen. Select returns from sockets_list, the stream that
+        is reader for input. So for example, if the server wants
+        to send a message, then the if condition will hold true
+        below.If the user wants to send a message, the else
+        condition will evaluate as true"""
+
+        read_sockets,write_socket, error_socket =\
+                          select.select(sockets_list,[],[],1)
+        print "select done"
+        for socks in read_sockets:
+            if socks == server:
+                message = socks.recv(2048)
+                print message
+                sleep(0.5)
+            else:
+                message = sys.stdin.readline()
+                server.send(message)
+                sys.stdout.write("<You>")
+                sys.stdout.write(message)
+                sys.stdout.flush()
+
+                
+        msg = "get " + NODE + ":led4\n"
+        server.send(msg)
+        print msg
+
+        TM.leds[l] = False
+        l = l+1
+        if l > 7:
+            l = 0
+        TM.leds[l] = True
         
-    a=TM.getData(0)
-    b=TM.getData(0)
-    if l == 2:
-        print b
-        print a
-        for x in a:
-            print x
+        a=TM.getData(0)
+        b=TM.getData(0)
+        if l == 2:
+            print b
+            print a
+            for x in a:
+                print x
             
-    # 	b=TM.getData(1)
-    TM.segments[0] = ''.join("%02d"%x for x in a)
-    # 	TM.segments[8] = ''.join("%02d" % x for x in b)
-    sleep(0.1)
+        # 	b=TM.getData(1)
+        TM.segments[0] = ''.join("%02d"%x for x in a)
+        # 	TM.segments[8] = ''.join("%02d" % x for x in b)
+        #sleep(0.1)
+
+
